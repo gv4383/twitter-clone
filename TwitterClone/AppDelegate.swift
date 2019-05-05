48b304c682a7350889c8a3d79ad1bf2a6a5fb4f1
gv4383 <gregory.vargas18@gmail.com>
2019-05-04 23:46:20 -0500

    Created header, footer, and body consisting of a list of text

--- TwitterClone/AppDelegate.swift
+++ TwitterClone/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // create new window instance that the app will live in
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let homeController = HomeController(collectionViewLayout: UICollectionViewFlowLayout())
+
+        window?.rootViewController = UINavigationController(rootViewController: homeController)
+        
         return true
     }
 
