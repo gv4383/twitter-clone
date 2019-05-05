157870e7233b31b93777995ae9058e215945c670
gv4383 <gregory.vargas18@gmail.com>
2019-05-04 21:54:42 -0500

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Goyo Vargas on 5/4/19.
+//  Copyright © 2019 SkwerlHub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
