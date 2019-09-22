6765f4523fca86383128cc9652e735eeecde0abd
Greg Vargas <greg.vargas@silvercar.com>
2019-09-22 15:51:28 -0500

    Created new separate view for tweets

--- TwitterClone/Model/HomeDatasource.swift
+++ TwitterClone/Model/HomeDatasource.swift
@@ -13,14 +13,14 @@ class HomeDataSource: Datasource {
     let users: [User] = {
         let goyoUser = User(name: "Goyo Var", username: "@goyoyo", bioText: "Good hello my doggies! I'm learning Swift to make iOS apps and it's hard, but also kinda fun actually.", profileImage: UIImage(named: "profile_image")!)
         
-        let tokaseUser = User(name: "Tokase Gokulo", username: "@tokagoku", bioText: "Tokase Gokulo is an iPhone developer and tweets on topics related to iPhone, software, and gaming. Check out our conference!", profileImage: UIImage(named: "other_image")!)
+        let tokaseUser = User(name: "Tokase Gokulo", username: "@tokagoku", bioText: "Tokase Gokulo is an iOS developer and tweets on topics related to iOS, software, and gaming. Check out our conference!", profileImage: UIImage(named: "other_image")!)
         
-        let crocsUser = User(name: "Crocs Lover", username: "crocs4lyfe", bioText: "Crocks ROCK. I absolutely love wearing crocs. Hot outside? Easy ventilation. Cold outside? Just wear some socks underneath. Going to a party? What better way to impress the crowd then to wear your crocs! I CANNOT express how much these things have changed my life... SERIOUSLY. GET. SOME. CROCS. NOW.", profileImage: UIImage(named: "other_image")!)
+        let crocsUser = User(name: "Crocs Lover", username: "@crocs4lyfe", bioText: "Crocks ROCK. I absolutely love wearing crocs. Hot outside? Easy ventilation. Cold outside? Just wear some socks underneath. Going to a party? What better way to impress the crowd then to wear your crocs! I CANNOT express how much these things have changed my life... SERIOUSLY. GET. SOME. CROCS. NOW.", profileImage: UIImage(named: "other_image")!)
         
-        return [goyoUser, tokaseUser, crocsUser]
+        return [tokaseUser, goyoUser, crocsUser]
     }()
     
-//    let words = ["user1", "user2", "user3"]
+    let tweets = ["tweet1", "tweet2"]
     
     override func headerClasses() -> [DatasourceCell.Type]? {
         return [UserHeader.self]
@@ -31,14 +31,22 @@ class HomeDataSource: Datasource {
     }
     
     override func cellClasses() -> [DatasourceCell.Type] {
-        return [UserCell.self]
+        return [UserCell.self, TweetCell.self]
     }
     
     override func item(_ indexPath: IndexPath) -> Any? {
         return users[indexPath.item]
     }
     
+    override func numberOfSections() -> Int {
+        return 2
+    }
+    
     override func numberOfItems(_ section: Int) -> Int {
+        if section == 1 {
+            return tweets.count
+        }
+        
         return users.count
     }
 }
---
+++ TwitterClone/View/TweetCell.swift
@@ -0,0 +1,31 @@
+//
+//  TweetCell.swift
+//  TwitterClone
+//
+//  Created by Greg Vargas on 9/22/19.
+//  Copyright © 2019 SkwerlHub. All rights reserved.
+//
+
+import LBTAComponents
+
+class TweetCell: DatasourceCell {
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "profile_image")
+        imageView.layer.cornerRadius = 5
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        separatorLineView.isHidden = false
+        separatorLineView.backgroundColor = UIColor(r: 230, g: 230, b: 230)
+        
+        backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
+    }
+}
